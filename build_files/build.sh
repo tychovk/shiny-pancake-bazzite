1053e314eaad820bbd8a406e3998f9ce5b3ee7a9
Tycho van Kleef <tychovk@gmail.com>
2026-03-04 16:51:33 +0100

    packages from forward slash to backslash

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,10 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y /
-tmux /
+dnf5 install -y \
+tmux \
 # Throttlestop: install msr-tools
-msr-tools /
+msr-tools \
 lutris
 # Div Acer Manager (Predator turbo and fan control) -- read install script and replicate on custom img
 # sudo curl -fsSL https://raw.githubusercontent.com/PXDiv/Div-Acer-Manager-Max/refs/heads/main/scripts/remoteSetup.sh -o /tmp/setup.sh && sudo bash /tmp/setup.sh
