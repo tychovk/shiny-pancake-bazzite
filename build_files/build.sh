65ff9218d077567a7fac02916e0365e075ed6118
Tycho van Kleef <tychovk@gmail.com>
2026-03-06 17:45:27 +0100

    Revert "build.sh back to default"
    This reverts commit 25c1006799599dcd43e92053cc564f67e5acc748.

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,12 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y tmux
-
+# Throttlestop: install msr-tools
+dnf5 install -y msr-tools systemd
+# App to manage game collection
+# dnf5 install -y lutris
+# Div Acer Manager (Predator turbo and fan control) -- read install script and replicate on custom img
+# sudo curl -fsSL https://raw.githubusercontent.com/PXDiv/Div-Acer-Manager-Max/refs/heads/main/scripts/remoteSetup.sh -o /tmp/setup.sh && sudo bash /tmp/setup.sh
 
 # Use a COPR Example:
 #
@@ -23,3 +28,5 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+# Throttlestop: add conf to load MSR kernel module
+# echo "msr" > /etc/modules-load.d/msr.conf
