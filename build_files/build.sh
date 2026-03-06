d48214f283ca496817f4976106587d0d0a5c1916
Tycho van Kleef <tychovk@gmail.com>
2026-03-06 16:05:41 +0100

    commented out lutris

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ dnf5 install -y tmux
 # Throttlestop: install msr-tools
 dnf5 install -y msr-tools
 # App to manage game collection
-dnf5 install -y lutris
+#dnf5 install -y lutris
 # Div Acer Manager (Predator turbo and fan control) -- read install script and replicate on custom img
 # sudo curl -fsSL https://raw.githubusercontent.com/PXDiv/Div-Acer-Manager-Max/refs/heads/main/scripts/remoteSetup.sh -o /tmp/setup.sh && sudo bash /tmp/setup.sh
 
